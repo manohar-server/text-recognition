7fae5c914632a0d583120a386da64c57a1c7fe87
Manohar <manubomma@gmail.com>
2018-11-21 17:55:14 +0530

    initial commit

---
+++ text-extractor-from-image/src/main/java/com/softfeels/imageprocessor/textextractorfromimage/exception/FileStorageException.java
@@ -0,0 +1,12 @@
+package com.softfeels.imageprocessor.textextractorfromimage.exception;
+
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ text-extractor-from-image/src/test/java/com/softfeels/imageprocessor/textextractorfromimage/TextExtractorFromImageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softfeels.imageprocessor.textextractorfromimage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextExtractorFromImageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
